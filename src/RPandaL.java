8f909c36082f7a61cb4396d7014a432116c89846
MrrRaph <dray.raph@gmail.com>
2021-11-03 14:27:52 +0100

    Refactored Calc in MultiThreaded Server RPL Calc

--- src/RPandaL.java
+++ src/RPandaL.java
@@ -1,7 +1,36 @@
 import parser.RPandaLParser;
 
+import java.io.IOException;
+import java.net.PortUnreachableException;
+import java.net.ServerSocket;
+
 public class RPandaL {
-    public static void main(String[] args) {
-        new RPandaLParser();
+    private static final int DEFAULT_PORT = 8080;
+    private int port;
+    private String host;
+
+    public RPandaL(final String host, final int port) {
+        this.host = host;
+        try {
+            if (port > 65536) {
+                this.port = DEFAULT_PORT;
+                throw new PortUnreachableException("Port higher than 65536");
+            }
+            this.port = port;
+            System.out.println("Listening on port " + this.port);
+        } catch (PortUnreachableException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public int actualPort() {
+        return this.port;
+    }
+
+    public static void main(String[] args) throws IOException {
+        RPandaL multiThreadedRPandaL = new RPandaL("localhost", 20000);
+       try (ServerSocket listener = new ServerSocket(multiThreadedRPandaL.actualPort())) {
+           while (true) new Thread(new RPandaLParser(listener.accept())).start();
+       }
     }
 }
--- src/parser/RPandaLParser.java
+++ src/parser/RPandaLParser.java
@@ -6,30 +6,46 @@ import stackable.Number;
 import stackable.Vector;
 import utils.numbers.NumHelper;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
-public class RPandaLParser {
+public class RPandaLParser implements Runnable {
+    private Socket socket;
     private Scanner in;
+    private PrintWriter out;
     private PileRPL stack;
 
-    public RPandaLParser() {
-        this.in = new Scanner(System.in);
+    public RPandaLParser(Socket socket) {
+        this.socket = socket;
         this.stack = new PileRPL();
+    }
 
-        String input;
-        while (!(input = in.nextLine()).equals("q")) {
-            switch (input) {
-                case "+" -> this.stack.add();
-                case "-" -> this.stack.sub();
-                case "*" -> this.stack.mul();
-                case "/" -> this.stack.div();
-                case "%" -> this.stack.mod();
-                case "C", "c", "CE", "ce" -> this.stack.clear();
+    @Override
+    public void run() {
+        try {
+            this.in = new Scanner(this.socket.getInputStream());
+            this.out = new PrintWriter(this.socket.getOutputStream(), true);
+
+            String input;
+            while (!(input = in.nextLine()).equals("q")) {
+                switch (input) {
+                    case "+" -> this.stack.add();
+                    case "-" -> this.stack.sub();
+                    case "*" -> this.stack.mul();
+                    case "/" -> this.stack.div();
+                    case "%" -> this.stack.mod();
+                    case "C", "c", "CE", "ce" -> this.stack.clear();
+                }
+                if (NumHelper.isNumeric(input)) this.stack.stack(new Number(Double.valueOf(input)));
+                if (NumHelper.isVector(input)) this.stack.stack(Vector.fromStr(input));
+                if (NumHelper.isComplex(input)) this.stack.stack(Complex.fromStr(input));
+                this.out.println(this.stack);
             }
-            if (NumHelper.isNumeric(input)) this.stack.stack(new Number(Double.valueOf(input)));
-            if (NumHelper.isVector(input)) this.stack.stack(Vector.fromStr(input));
-            if (NumHelper.isComplex(input)) this.stack.stack(Complex.fromStr(input));
-            System.out.println(this.stack);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
